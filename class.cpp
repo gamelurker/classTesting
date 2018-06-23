103c0aa5a2138bd75032342d25075ff5ccb51a75
vadim.gr.2012@yandex.ru <vadim.gr.2012@yandex.ru>
2018-06-23 17:04:06 +0300

    Изменил файл CPP, добавиви тестовое задание из mera

--- class.cpp
+++ class.cpp
@@ -1,20 +1,19 @@
 #include "iostream"
 using namespace std;
-class Vector3
+class A
 {
-	int x, y, z;
 public:
-	Vector3(){
-	}
-	Vector3(int x, int y, int z) : x(x), y(y), z(z)
-	{}
-	Vector3 operator = (Vector3 v1){
-		return Vector3(this->x = v1.x, this->y = v1.y, this->z = 0 );
+	 void a (){ cout << "a";
+	 }
+};
+class  B{
+public: 
+	void b(){cout << "b";
 	}
 };
 int main(){
-Vector3 v1(1,2,3), v2(0,0,0), v3();
-v3 = v1;
-cout << v3(); 
+B b;
+b.b();
+
 }
 
